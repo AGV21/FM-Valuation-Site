fea99445a3940cd39258d55280b6891a9d0999a3
Gabriel <alexgv2122@gmail.com>
2023-03-03 14:01:28 -0500

    added form where theres a drop down to pick positions and has stats to fill in.

--- index.php
+++ index.php
@@ -1,8 +1,47 @@
 <html>
     <head>
         <title>FM Valuation</title>
+        <link href="styles.css" rel="stylesheet">
     </head>
-    <body id="mainBody" >
-        <p>Is this working</p>
+    <body id="mainContainer">
+        <div class="trans">
+            <h1>Player Valuation</h1>
+            <form>
+                <select>
+                    <option value="ST">ST</option>
+                    <option value="Winger">Winger</option>
+                    <option value="CAM">CAM</option>
+                    <option value="CM">CM</option>
+                    <option value="DM">DM</option>
+                    <option value="WB">WB</option>
+                    <option value="FB">FB</option>
+                    <option value="CB">CB</option>
+                    <option value="SK">SK</option>
+                    <option value="GK">GK</option>
+                </select>
+                <label for="Goals">Goals:</label>
+                <input type="number">
+                <label for="Penalties">Penalties:</label>
+                <input type="number">
+                <label for="xG">xG:</label>
+                <input type="number">
+                <label for="NPxG">NP-xG:</label>
+                <input type="number">
+                <label for="Shot%">Shot%</label>
+                <input type="number"><br>
+                <label for="Pass%">Pass%:</label>
+                <input type="number">
+                <label for="Tackles">Tackles:</label>
+                <input type="number">
+                <label for="Chances created">Chances created:</label>
+                <input type="number">
+                <label for="Distance cover">Distance cover:</label>
+                <input type="number"><br>
+                <label for="Tackles">Tackles:</label>
+                <input type="number">
+                <label for="clearences">Clearences:</label>
+                <input type="number">
+
+        </div>
     </body>
 </html>
