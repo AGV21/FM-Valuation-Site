4b757c6d0eb1a9cac3dd116e7bb5bef164ccb3cc
Giancarlo Lomanto <gc33791@gmail.com>
2023-03-15 13:18:11 -0400

    replace labels and added placeholders for dropdown

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
     <head>
         <title>FM Valuation</title>
@@ -8,8 +9,9 @@
             <h1>Player Valuation</h1>
             <form>
                 <select>
+                    <option>Position</option>
                     <option value="ST">ST</option>
-                    <option value="Winger">Winger</option>
+                    <option value="W">W</option>
                     <option value="CAM">CAM</option>
                     <option value="CM">CM</option>
                     <option value="DM">DM</option>
@@ -18,30 +20,41 @@
                     <option value="CB">CB</option>
                     <option value="SK">SK</option>
                     <option value="GK">GK</option>
-                </select>
-                <label for="Goals">Goals:</label>
-                <input type="number">
-                <label for="Penalties">Penalties:</label>
-                <input type="number">
-                <label for="xG">xG:</label>
-                <input type="number">
-                <label for="NPxG">NP-xG:</label>
-                <input type="number">
-                <label for="Shot%">Shot%</label>
-                <input type="number"><br>
-                <label for="Pass%">Pass%:</label>
-                <input type="number">
-                <label for="Tackles">Tackles:</label>
-                <input type="number">
-                <label for="Chances created">Chances created:</label>
-                <input type="number">
-                <label for="Distance cover">Distance cover:</label>
-                <input type="number"><br>
-                <label for="Tackles">Tackles:</label>
-                <input type="number">
-                <label for="clearences">Clearences:</label>
-                <input type="number">
+                </select><br>
+                <select>
+                    <option>League Rating</option>
+                    <option value="5">5 Stars</option>
+                    <option value="4.5">4.5 Stars</option>
+                    <option value="4">4 Stars</option>
+                    <option value="3.5">3.5 Stars</option>
+                    <option value="3">3 Stars</option>
+                    <option value="2.5">2.5 Stars</option>
+                    <option value="2">2 Stars</option>
+                    <option value="1.5">1.5 Stars</option>
+                    <option value="1">1 Stars</option>
+                    <option value=".5">.5 Stars</option>
+                </select><br>
 
+                <label for="Minutes">Minutes:</label>
+                <input type="numbers"><br>
+                <label for="xG per 90">xG per 90:</label>
+                <input type="number"><br>
+                <label for="Non-penalty xG per 90">Non-penalty xG per 90:</label>
+                <input type="number"><br>
+                <label for="Goals per 90">Goals per 90:</label>
+                <input type="number"><br>
+                <label for="Non-penalty goals per 90">Non-penalty goals per 90:</label>
+                <input type="number"><br>
+                <label for="Goals per 90">Goals per 90:</label>
+                <input type="number"><br>
+                <label for="Shots per 90">Shots per 90:</label>
+                <input type="number"><br>
+                <label for="Shot Ratio">Shot Ratio</label>
+                <input type="number"><br>
+                <label for="Key passes per 90">Key passes per 90:</label>
+                <input type="number"><br>
+                <label for="Coversion rate">Coversion rate:</labek>
+                <input type="number">
         </div>
     </body>
 </html>
